dcc422271c10302706cb82d9b2de0e2f1537278b
suman-samanta <sumansamanta110@gmail.com>
2023-07-20 17:53:16 +0530

    hi

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1/todo_list_db');
+mongoose.connect('mongodb+srv://user31:test123@cluster0.ycvqo.mongodb.net/habitTracker?retryWrites=true&w=majority');
 const db = mongoose.connection;
 // error
 db.on('error',console.error.bind(console,'erroe connecting to db'));
